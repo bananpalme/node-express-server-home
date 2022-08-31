2eca4f74378c57f9ab3a7dea362491aff12c4959
bananpalme <77321863+bananpalme@users.noreply.github.com>
2022-08-31 11:31:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//hent biblioteket IP 
+const ip = require('ip')
+//fordi vi har hentet biblioteket IP kan vi hurtoigt få fat på ip adressen
+console.log(ip.address())
+//hent biblioteket express og gem objektet i en konstant
+const express = require('express')
+//opret en variabel til express serveren
+const app = express()
+//definer en port
+const port = 4040
+
+//serve en statisk mappe i roden 
+app.use('/', express.static('public'))
+
+//Hvis der kommer klienter til endpointet /hvaler
+app.get('/api/hvaler', (req, res)=>{
+    res.send('Du er kommet til mit hval api')
+})
+
+//serveren skal bare sidde og køre på porten
+app.listen(port, ()=>{
+    console.log('Server lytter på port: ' + port)
+}) 
